403ad48f261e41257b98d77e1984ac9fe35b5711
Dhiraj456186 <drai6186@gmail.com>
2025-07-22 14:46:04 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "32"; // can be converted into number 32
+//let score = "342abc"; // on conversion to number it gives NaN
+//  true => on converting gives 1, false => 0
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+let isLoggedIn = 1 // converted to true
+// 0 => false
+// empty string "" => false
+// "Dhiraj" => true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
