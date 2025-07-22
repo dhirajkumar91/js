ea2311192ba6fa36a68260b6194e59af6bd173ac
Dhiraj456186 <drai6186@gmail.com>
2025-07-22 15:05:55 +0530

    Added Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+// Conversion of data types
+
 let score = "32"; // can be converted into number 32
 //let score = "342abc"; // on conversion to number it gives NaN
 //  true => on converting gives 1, false => 0
@@ -25,5 +27,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ***************** Operations *****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //power
+// console.log(2/2);
+// console.log(2%2); // remainder
+
+
+
+let str1 = "hello";
+let str2 = " dhiraj";
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1+ 2 + "2"); // 32
+
+console.log(true); // true
+console.log(+true); // 1 => means + likhne se conversion hota hai
+console.log(+""); // 0
