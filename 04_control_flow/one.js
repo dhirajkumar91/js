6bfee8b0e237bbf1869df63c85512505aa0463ec
Dhiraj456186 <drai6186@gmail.com>
2025-07-24 23:52:10 +0530

    Control flow basics

---
+++ 04_control_flow/one.js
@@ -0,0 +1,16 @@
+// if statement
+
+const isUserLoggedIn = true
+const temperature = 42
+
+if ( temperature < 50) {
+    //console.log("less than 50");
+}
+// console.log("temperature is greater than 50");
+
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,39 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default: 
+        console.log("default case match");
+        break;
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
